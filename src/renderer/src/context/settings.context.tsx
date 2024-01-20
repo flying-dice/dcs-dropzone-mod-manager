4d0d1a4db13d55d2726c3568312f38c68d909838
🐸 <dice-a6db@outlook.com>
2024-01-20 15:00:48 +0000

    fix: add error message when settings context fails to load

--- src/renderer/src/context/settings.context.tsx
+++ src/renderer/src/context/settings.context.tsx
@@ -1,9 +1,18 @@
-import { createContext, FC, ReactNode, useContext } from 'react'
-import { makeAutoObservable } from 'mobx'
-import { config } from '../config'
-import { client } from '../client'
-import { useAsync } from 'react-use'
-import { LoadingOverlay } from '@mantine/core'
+import { createContext, FC, ReactNode, useContext } from "react";
+import { makeAutoObservable } from "mobx";
+import { config } from "../config";
+import { client } from "../client";
+import { useAsync } from "react-use";
+import { Alert, List, LoadingOverlay } from "@mantine/core";
+
+class SettingsError extends Error {
+  errors: string[]
+
+  constructor(message: string, errors: string[] = []) {
+    super(message)
+    this.errors = errors
+  }
+}
 
 export class Settings {
   registryUrl: string
@@ -23,7 +32,14 @@ export class Settings {
       (await client.installation.getDefaultWriteDir.query().then((it) => it.path))
 
     if (!registryUrl || !writeDir) {
-      throw new Error('Could not load settings from local storage')
+      const error = new SettingsError('Could not load settings from local storage.')
+      if (!registryUrl) {
+        error.errors.push('Registry url could not be loaded.')
+      }
+      if (!writeDir) {
+        error.errors.push('Write directory could not be loaded.')
+      }
+      throw error
     }
 
     return new Settings({ writeDir, registryUrl })
@@ -48,7 +64,22 @@ export const SettingsProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
   return (
     <>
-      <LoadingOverlay visible={settings.loading} overlayProps={{ blur: 2 }} />
+      <LoadingOverlay
+        visible={Boolean(settings.loading || settings.error)}
+        overlayProps={{ blur: 2 }}
+        loaderProps={{
+          children: settings.error && (
+            <Alert color={'red'} title={'Could not load settings'}>
+              {settings.error.message}
+              <List>
+                {(settings.error as SettingsError).errors.map((it) => (
+                  <List.Item key={it}>{it}</List.Item>
+                ))}
+              </List>
+            </Alert>
+          )
+        }}
+      />
       {settings.value && (
         <SettingsContext.Provider value={settings.value}>{children}</SettingsContext.Provider>
       )}
