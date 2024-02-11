d3415ae9e7b842c36bae053e6744db73dcee79d7
john681611 <john681611@hotmail.com>
2024-02-11 11:21:15 +0000

    Support Single download to multiple setup

--- src/main/features/installation.ts
+++ src/main/features/installation.ts
@@ -1,91 +1,195 @@
 import { trpc } from '../trpc'
 import { z } from 'zod'
-import { EntryInstallMap, EntryInstallMapSchema, EntryInstallState } from '../../client';
-import fs from "fs";
-import fsp from "fs/promises";
-import workerpool from  'workerpool'
+import { EntryInstallMap, EntryInstallMapSchema, EntryInstallState } from '../../client'
+import fs from 'fs'
+import fsp from 'fs/promises'
+import workerpool from 'workerpool'
 import { getInstalledFilePath, getSymlinkFilePath } from '../utils'
+import { join } from 'path'
+
+export type installWorkerMessage = {
+  status: string
+}
 
 
 const fileInstallStatus = {} //is this a dumb in memory download status why yes it is!
-const baseEntrySchema = z.object({modId: z.string(), installMapArr: EntryInstallMapSchema, writeDirPath: z.string(), saveDirPath: z.string()})
+const baseEntrySchema = z.object({
+  modId: z.string(),
+  installMapArr: EntryInstallMapSchema,
+  writeDirPath: z.string(),
+  saveDirPath: z.string()
+})
 
-const getInstallState = async (modId: string, installMapArr: EntryInstallMap[],  writeDirPath: string, saveDirPath: string): Promise<EntryInstallState> => {
-  const installStates = installMapArr.map(x => ({name: getInstalledFilePath(modId, writeDirPath, x), installed: fs.existsSync(getInstalledFilePath(modId, writeDirPath, x))}))
-  const linkStates = installMapArr.map(x => ({enabled: fs.existsSync(getSymlinkFilePath(saveDirPath, x))}))
+const getInstallState = async (
+  modId: string,
+  installMapArr: EntryInstallMap[],
+  writeDirPath: string,
+  saveDirPath: string
+): Promise<EntryInstallState> => {
+  const installStates = installMapArr.map((x) => ({
+    name: getInstalledFilePath(modId, writeDirPath, x),
+    installed: fs.existsSync(getInstalledFilePath(modId, writeDirPath, x))
+  }))
+  const linkStates = installMapArr.map((x) => ({
+    enabled: fs.existsSync(getSymlinkFilePath(saveDirPath, x))
+  }))
 
   return {
-    installed: installStates.some(x => x.installed),
-    installedVersion: "1", // TODO: actually check somehow
-    incomplete: installStates.every(x => !x.installed),
-    missingFiles: installStates.filter(x => !x.installed).map(x => x.name),
-    enabled: linkStates.some(x => x.enabled),
+    installed: installStates.some((x) => x.installed),
+    installedVersion: '1', // TODO: actually check somehow
+    incomplete: installStates.every((x) => !x.installed),
+    missingFiles: installStates.filter((x) => !x.installed).map((x) => x.name),
+    enabled: linkStates.some((x) => x.enabled)
   }
 }
 
-const enableMod = async(modId: string, installMapArr: EntryInstallMap[], writeDirPath: string, saveDirPath: string): Promise<EntryInstallState> => {
-  await Promise.all(installMapArr.map(async (x) => {
-      const installedFilePath = getInstalledFilePath(modId, writeDirPath, x);
+const enableMod = async (
+  modId: string,
+  installMapArr: EntryInstallMap[],
+  writeDirPath: string,
+  saveDirPath: string
+): Promise<EntryInstallState> => {
+  await Promise.all(
+    installMapArr.map(async (x) => {
+      const installedFilePath = getInstalledFilePath(modId, writeDirPath, x)
       const symlinkFilePath = getSymlinkFilePath(saveDirPath, x)
       const fileStats = await fsp.stat(installedFilePath)
-      return fsp.symlink(installedFilePath, symlinkFilePath, fileStats.isFile() ? 'file':'junction')
-  }))
-  return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath);
+      return fsp.symlink(
+        installedFilePath,
+        symlinkFilePath,
+        fileStats.isFile() ? 'file' : 'junction'
+      )
+    })
+  )
+  return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath)
 }
 
-const disableMod = async(modId: string, installMapArr: EntryInstallMap[], writeDirPath: string, saveDirPath: string): Promise<EntryInstallState> => {
-  await Promise.all(installMapArr.map(async (x) => {
+const disableMod = async (
+  modId: string,
+  installMapArr: EntryInstallMap[],
+  writeDirPath: string,
+  saveDirPath: string
+): Promise<EntryInstallState> => {
+  await Promise.all(
+    installMapArr.map(async (x) => {
       return fsp.unlink(getSymlinkFilePath(saveDirPath, x))
-  }))
-  return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath);
+    })
+  )
+  return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath)
 }
 
-const pool = workerpool.pool('./src/main/workers/installMod.js');
+const pool = workerpool.pool('./src/main/workers/installMod.js')
 
 export const installationService = {
   async installMod(modId: string, installMapArr: EntryInstallMap[], writeDirPath: string) {
-    installMapArr.map((installMap: EntryInstallMap) => {
-      const stateKey = `${modId}-${getInstalledFilePath(modId, writeDirPath, installMap)}`
-      fileInstallStatus[stateKey] = "preparing"
-      pool.exec('downloadAndUnzip', [modId, installMap, writeDirPath], {
-      on: (payload: any) => {
-        fileInstallStatus[stateKey] = payload.status
+    const aggInstallMaps = {}
+    installMapArr.forEach((installMap: EntryInstallMap) => {
+      const key = installMap.source.split('/#/')[0]
+      if (!aggInstallMaps[key]) {
+        aggInstallMaps[key] = []
       }
-    }).catch(err => {
-      console.error(err)
-      fileInstallStatus[stateKey] = err;
+      aggInstallMaps[key].push(installMap)
     })
-  });
+    Object.keys(aggInstallMaps).map((key: string, idx) => {
+      const installArr = aggInstallMaps[key]
+      const stateKey = `${modId}${idx > 0 ? idx + 1 : ''}`
+      fileInstallStatus[stateKey] = 'preparing'
+      pool
+        .exec('downloadAndUnzip', [modId, key, installArr, writeDirPath], {
+          on: (payload: installWorkerMessage) => {
+            fileInstallStatus[stateKey] = payload.status
+          }
+        })
+        .catch((err) => {
+          console.error(err)
+          fileInstallStatus[stateKey] = err
+        })
+    })
   },
-  async uninstallMod(modId: string, installMapArr: EntryInstallMap[], writeDirPath: string, saveDirPath: string): Promise<EntryInstallState> {
+  async uninstallMod(
+    modId: string,
+    installMapArr: EntryInstallMap[],
+    writeDirPath: string,
+    saveDirPath: string
+  ): Promise<EntryInstallState> {
     try {
       await disableMod(modId, installMapArr, writeDirPath, saveDirPath)
-    } catch (e) {}
-   
-    await Promise.all(installMapArr.map(x => {
-      return fsp.rm(getInstalledFilePath(modId, writeDirPath, x), { recursive: true, force: true })
-    }))
-    return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath);
+    } catch (e) {
+      console.error('Failed to disable mod', e)
+    }
+
+    await fsp.rm(join(writeDirPath, modId), {
+      recursive: true,
+      force: true
+    })
+
+    return await getInstallState(modId, installMapArr, writeDirPath, saveDirPath)
   },
   getInstallProgress(): Record<string, string> {
     return fileInstallStatus
   },
   clearProgress() {
-    Object.keys(fileInstallStatus).filter(x => fileInstallStatus[x].endsWith("Complete")).forEach(key => delete fileInstallStatus[key])
+    Object.keys(fileInstallStatus)
+      .filter((x) => fileInstallStatus[x].endsWith('Complete'))
+      .forEach((key) => delete fileInstallStatus[key])
   },
   getInstallState,
   enableMod,
-  disableMod,
+  disableMod
 }
 
-
-
 export const installationRouter = trpc.router({
-  getInstallState: trpc.procedure.input(baseEntrySchema).query(({ input }) => installationService.getInstallState(input.modId, input.installMapArr, input.writeDirPath, input.saveDirPath)),
+  getInstallState: trpc.procedure
+    .input(baseEntrySchema)
+    .query(({ input }) =>
+      installationService.getInstallState(
+        input.modId,
+        input.installMapArr,
+        input.writeDirPath,
+        input.saveDirPath
+      )
+    ),
   getInstallProgress: trpc.procedure.query(installationService.getInstallProgress),
   clearProgress: trpc.procedure.query(installationService.clearProgress),
-  installMod: trpc.procedure.input(z.object({modId: z.string(), installMapArr: EntryInstallMapSchema, writeDirPath: z.string()})).query(({ input }) => installationService.installMod(input.modId, input.installMapArr, input.writeDirPath)),
-  uninstallMod: trpc.procedure.input(baseEntrySchema).query(({ input }) => installationService.uninstallMod(input.modId, input.installMapArr, input.writeDirPath, input.saveDirPath)),
-  enableMod: trpc.procedure.input(baseEntrySchema).query(({ input }) => installationService.enableMod(input.modId, input.installMapArr, input.writeDirPath, input.saveDirPath)),
-  disableMod: trpc.procedure.input(baseEntrySchema).query(({ input }) => installationService.disableMod(input.modId, input.installMapArr, input.writeDirPath, input.saveDirPath)),
+  installMod: trpc.procedure
+    .input(
+      z.object({
+        modId: z.string(),
+        installMapArr: EntryInstallMapSchema,
+        writeDirPath: z.string()
+      })
+    )
+    .query(({ input }) =>
+      installationService.installMod(input.modId, input.installMapArr, input.writeDirPath)
+    ),
+  uninstallMod: trpc.procedure
+    .input(baseEntrySchema)
+    .query(({ input }) =>
+      installationService.uninstallMod(
+        input.modId,
+        input.installMapArr,
+        input.writeDirPath,
+        input.saveDirPath
+      )
+    ),
+  enableMod: trpc.procedure
+    .input(baseEntrySchema)
+    .query(({ input }) =>
+      installationService.enableMod(
+        input.modId,
+        input.installMapArr,
+        input.writeDirPath,
+        input.saveDirPath
+      )
+    ),
+  disableMod: trpc.procedure
+    .input(baseEntrySchema)
+    .query(({ input }) =>
+      installationService.disableMod(
+        input.modId,
+        input.installMapArr,
+        input.writeDirPath,
+        input.saveDirPath
+      )
+    )
 })
--- src/main/workers/installMod.js
+++ src/main/workers/installMod.js
@@ -1,62 +1,78 @@
-const  Downloader =  require("nodejs-file-downloader")
-const  unzipper =  require('unzipper')
-const  { join } =  require("path");
-const  fs =  require("fs")
-const  fsp =  require("fs/promises")
+const Downloader = require('nodejs-file-downloader')
+const unzipper = require('unzipper')
+const { join } = require('path')
+const fs = require('fs')
+const fsp = require('fs/promises')
 
-const workerpool = require('workerpool');
+const workerpool = require('workerpool')
 const HASH_SPLIT = '/#/'
 
-const getInstalledFilePath = (modId ,basePath, installMap) => {
+const getInstallFileLocalPath = (installMap) => {
   let installFile = installMap.source
-  if(installFile.includes(HASH_SPLIT)) {
+  if (installFile.includes(HASH_SPLIT)) {
     installFile = installFile.split(HASH_SPLIT)[1]
   }
-  return join(basePath, modId, installFile)
+  return installFile
 }
 
-const downloadAndUnzip = async (modId, installMap, installBasePath) => {
-  let downloadPath = installMap.source
-  let installFile = installMap.source
-  if(installFile.includes(HASH_SPLIT)) {
-    const  parts = installFile.split(HASH_SPLIT)
-    installFile = parts[1]
-    downloadPath = parts[0]
-  }
-    const download = new Downloader({
-      url: downloadPath,
-      directory: join(installBasePath, modId),
-      skipExistingFileName: downloadPath.endsWith(".zip"), // as zips are deleted in cleanup with can do this to skip a step if errors laterp
-      onProgress: function (percentage) {
-        workerpool.workerEmit({
-            status: `${percentage}%`,
-          });
-      },
-    });
-    await download.download();
-    if(downloadPath.endsWith(".zip"))
-    {
-        workerpool.workerEmit({
-            status: 'Unpacking',
-          });
-        const downloadedFile = downloadPath.split('/').pop();
-        const downloadedFileUnzipped = downloadedFile.replace(".zip", "")
-      await fs.createReadStream(join(installBasePath, modId, downloadedFile)) 
-        .pipe(unzipper.Extract({ path: join(installBasePath, modId,downloadedFileUnzipped)})) // this is dumb as rocks don't like it would be nice if I could get a subset
-        .promise()
-      await fsp.rm(join(installBasePath, modId, downloadedFile),  { recursive: true, force: true })
-  
-      if(downloadedFileUnzipped !== installFile) {
-        await fsp.rename(join(installBasePath, modId, downloadedFileUnzipped, installFile), getInstalledFilePath(modId, installBasePath, installMap))
-        await fsp.rm(join(installBasePath, modId, downloadedFileUnzipped),  { recursive: true, force: true })
-      } 
-    }
-    workerpool.workerEmit({
-        status: 'Complete',
-      });
-    return getInstalledFilePath(modId, installBasePath, installMap)
+const getInstalledFilePath = (modId, basePath, installMap) =>
+  join(basePath, modId, getInstallFileLocalPath(installMap))
+
+const downloadAndUnzip = async (modId, downloadUrl, installMapArr, installBasePath) => {
+  const { filePath } = await download(modId, downloadUrl, installBasePath)
+  if (filePath.endsWith('.zip')) {
+    const unzippedFilePath = await unzip(filePath)
+    await unpack(modId, unzippedFilePath, installBasePath, installMapArr)
+    await fsp.rm(join(installBasePath, modId, unzippedFilePath), { //Potential edge case if unzipped is all we need but deal with it later :D
+      recursive: true,
+      force: true
+    })
   }
 
-  workerpool.worker({
-    downloadAndUnzip: downloadAndUnzip,
-  });
+  workerpool.workerEmit({
+    status: 'Complete'
+  })
+}
+
+const download = async (modId, downloadUrl, installBasePath) => {
+  const download = new Downloader({
+    url: downloadUrl,
+    directory: join(installBasePath, modId),
+    skipExistingFileName: downloadUrl.endsWith('.zip'), // as zips are deleted in cleanup with can do this to skip a step if errors later
+    onProgress: function (percentage) {
+      workerpool.workerEmit({
+        status: `${percentage}%`
+      })
+    }
+  })
+  return await download.download()
+}
+
+const unzip = async (downloadedFilePath) => {
+  workerpool.workerEmit({
+    status: 'Unpacking'
+  })
+  const downloadedFileUnzipped = downloadedFilePath.replace('.zip', '')
+  await fs
+    .createReadStream(downloadedFilePath)
+    .pipe(unzipper.Extract({ path: downloadedFileUnzipped })) // this is dumb as rocks don't like it would be nice if I could get a subset
+    .promise()
+  await fsp.rm(downloadedFilePath, { recursive: true, force: true })
+  return downloadedFileUnzipped
+}
+
+const unpack = async (modId, unzippedFilePath, installBasePath, installMapArr) => {
+  return await Promise.all(
+    installMapArr.map(async (installMap) => {
+      const installFile = getInstallFileLocalPath(installMap)
+      return await fsp.rename(
+        join(unzippedFilePath, installFile),
+        getInstalledFilePath(modId, installBasePath, installMap)
+      )
+    })
+  )
+}
+
+workerpool.worker({
+  downloadAndUnzip: downloadAndUnzip
+})
