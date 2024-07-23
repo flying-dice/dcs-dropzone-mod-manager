79c60593e0e052fa3a6af5f3cfd3cc2f1a915a28
🎲 <dice-a6db@outlook.com>
2024-07-23 12:24:25 +0100

    chore: remove redundant types

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,5 @@
 import { getAppRouter } from './main/router'
-import { z } from 'zod'
-import { EntryIndex, EntryLatestRelease } from './client'
 import { inferAsyncReturnType } from '@trpc/server'
-
-export const EntryInstallMapSchema = z.array(
-  z.object({
-    source: z.string(),
-    target: z.string()
-  })
-)
-
-export type EntryInstallMap = z.infer<typeof EntryInstallMapSchema>
-
-export interface EntryInstallState {
-  installed: boolean
-  installedVersion: string
-  incomplete: boolean
-  latestRelease: EntryLatestRelease
-  entry: EntryIndex
-  missingFiles: string[]
-  enabled: boolean
-}
-
-export interface EachEntryInstallState {
-  id: string
-  installMapArr: EntryInstallMap
-  installState: EntryInstallState
-  version: string
-}
 
 export type TaskState = 'Pending' | 'In Progress' | 'Completed' | 'Failed'
 
