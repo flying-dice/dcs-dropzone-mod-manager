9ecaf80a61ea672cecbe4a667a0cf8d65a383bf5
john681611 <john681611@hotmail.com>
2024-12-14 00:19:47 +0000

    Added quick links to external sites

--- src/renderer/src/components/app-nav.tsx
+++ src/renderer/src/components/app-nav.tsx
@@ -6,6 +6,7 @@ export type AppNavItem = {
   path: string
   tooltip: string
   icon: React.ReactNode
+  isExternal?: boolean
 }
 export type AppNavProps = {
   items: AppNavItem[]
@@ -14,11 +15,17 @@ export const AppNav: React.FC<AppNavProps> = ({ items }) => {
   const navigate = useNavigate()
   return (
     <Stack gap={0}>
-      {items.map(({ path, icon, tooltip }) => (
+      {items.map(({ path, icon, tooltip, isExternal }) => (
         <Tooltip key={path} label={tooltip} openDelay={500}>
-          <ActionIcon radius={0} size={'xl'} variant={'subtle'} onClick={() => navigate(path)}>
-            {icon}
-          </ActionIcon>
+          {isExternal ? (
+            <ActionIcon component="a" href={path} radius={0} size={'xl'} variant={'subtle'}>
+              {icon}
+            </ActionIcon>
+          ) : (
+            <ActionIcon radius={0} size={'xl'} variant={'subtle'} onClick={() => navigate(path)}>
+              {icon}
+            </ActionIcon>
+          )}
         </Tooltip>
       ))}
     </Stack>
