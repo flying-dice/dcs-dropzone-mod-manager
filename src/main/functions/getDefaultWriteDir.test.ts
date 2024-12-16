76366e6c9396046f4a2af1fcfa3418bc9e5d7f2b
🎲 <dice-a6db@outlook.com>
2024-12-16 18:38:18 +0000

    fix: refactor getDefaultWriteDir test to use mock config

--- src/main/functions/getDefaultWriteDir.test.ts
+++ src/main/functions/getDefaultWriteDir.test.ts
@@ -1,29 +1,16 @@
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
-import { config } from '../config'
+import { describe, it, expect, vi } from 'vitest'
+import { join } from 'node:path'
 import { getDefaultWriteDir } from './getDefaultWriteDir'
 
-const testLocalAppData = 'C:\\Users\\TestUser\\AppData\\Local'
-
-vi.mock('electron', () => ({
-  app: {
-    getPath: vi.fn().mockImplementation(() => 'C:\\Users\\TestUser\\AppData\\Local')
+vi.mock('../config', () => ({
+  config: {
+    writeDir: 'mockWriteDir'
   }
 }))
 
 describe('getDefaultWriteDir', () => {
-  let _LOCALAPPDATA: string | undefined
-
-  beforeAll(() => {
-    _LOCALAPPDATA = process.env.LOCALAPPDATA
-    process.env.LOCALAPPDATA = testLocalAppData
-  })
-
-  afterAll(() => {
-    process.env.LOCALAPPDATA = _LOCALAPPDATA
-  })
-
-  it('should return the directory path using LOCALAPPDATA if set', () => {
+  it('should return the default write directory path', () => {
     const result = getDefaultWriteDir()
-    expect(result).toBe(`${testLocalAppData}\\${config.appDataName}\\mods`)
+    expect(result).toBe(join('mockWriteDir', 'mods'))
   })
 })
